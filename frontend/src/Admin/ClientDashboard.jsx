09a330857bbeb0fecf1fdb8f8e9a08ab327de451
Steven <sc201789@ucf.edu>
2025-05-01 17:10:10 -0400

    address at the top of qr code pdf

--- frontend/src/Admin/ClientDashboard.jsx
+++ frontend/src/Admin/ClientDashboard.jsx
@@ -134,9 +134,9 @@ function ClientDashboard() {
     const doc = new jsPDF();
     
     doc.setFontSize(18);
-    doc.text(`Scan this QR Code for the employee log form.`, 20, 20);
     doc.text(`Address: ${qrCodeAddress}`, 20, 30);
-
+    doc.text(`Scan this QR Code for the employee log form.`, 20, 20);
+    
     doc.addImage(qrCode, 'PNG', 20, 30, 100, 100);
 
     doc.save(`qr-code-${qrCodeAddress}.pdf`);
