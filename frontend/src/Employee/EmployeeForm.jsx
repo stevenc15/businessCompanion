06dc85ef4a830770aa3121cd005b4d12c8e33b9e
Steven <sc201789@ucf.edu>
2025-05-02 20:05:57 -0400

    read only prefilled inputs

--- frontend/src/Employee/EmployeeForm.jsx
+++ frontend/src/Employee/EmployeeForm.jsx
@@ -137,7 +137,7 @@ function EmployeeForm() {
                 id="Community"
                 name="Community" 
                 placeholder="Enter Community Name" 
-                //onChange={handleChange}
+                readOnly
                 value={clientData?.Community || formData.Community}
                 className="form-input"
               />
@@ -150,7 +150,7 @@ function EmployeeForm() {
                 id="ClientName"
                 name="ClientName" 
                 placeholder="Enter Client Name" 
-                //onChange={handleChange}
+                readOnly
                 value={clientData?.ClientName || formData.ClientName}
                 className="form-input"
               />
@@ -163,7 +163,7 @@ function EmployeeForm() {
                 id="Address"
                 name="Address" 
                 placeholder="Enter full Address" 
-                //onChange={handleChange}
+                readOnly
                 value={clientData?.Address || formData.Address}
                 className="form-input"
               />
