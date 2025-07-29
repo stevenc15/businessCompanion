216344c42b7ce1ebbaff5a4943ae682bd25d4726
Steven <sc201789@ucf.edu>
2025-07-29 14:52:57 -0400

    removing admin add activity

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import ActivityDashboard from './Admin/ActivityDashboard';
 import ClientDashboard from './Admin/ClientDashboard';
 import AdminLogin from './Admin/AdminLogin';
 import AddClient from './Admin/AddClient';
-import AddActivity from './Admin/AddActivity';
 import EndUserAgreement from './EndUserAgreement';
 
 function App() {
@@ -19,7 +18,6 @@ function App() {
         <Route path="/activityDashboard" element={<ActivityDashboard/>} />
         <Route path="/clientDashboard" element={<ClientDashboard/>} />
         <Route path="/addClient" element={<AddClient/>} />
-        <Route path="/addActivity" element={<AddActivity/>} />
         <Route path="/endUserAgreement" element={<EndUserAgreement/>} />
       </Routes>
     </Router>
