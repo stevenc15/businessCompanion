b4c42728158bfc77fdd00ac20f5545f052afe8f0
Steven <sc201789@ucf.edu>
2025-12-19 20:51:49 -0500

    changed hook to a basic function

--- frontend/src/Employee/hooks/changeForm.jsx
+++ frontend/src/Employee/hooks/changeForm.jsx
@@ -3,13 +3,11 @@
  * custom hook to handle changes in the employee form
  */
 
-export default function useChangeForm(formData, setFormData, e) {
-    return function handleChange(e) {
+export default function changeForm(setFormData, e) {
       const {name, value, type, checked} = e.target;
 
       setFormData(prev => ({
         ...prev, 
        [name]: type === 'checkbox' ? checked:value
       }));
-    };
-  }
+    }
