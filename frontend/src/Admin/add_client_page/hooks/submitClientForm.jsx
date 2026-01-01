b37aa83591e8901456770b920edadce4a6f4363c
Steven <sc201789@ucf.edu>
2026-01-01 14:57:12 -0500

    fixing some previous merge errors

--- frontend/src/Admin/add_client_page/hooks/submitClientForm.jsx
+++ frontend/src/Admin/add_client_page/hooks/submitClientForm.jsx
@@ -26,25 +26,3 @@ export default async function submitClientForm(e, formData, setFormData) {
 /**
  * submitClientForm.jsx - hook to handle submission of add client form 
  */
-
-import { CLIENTFORMCONFIG } from "../config/clientForm";
-import { API_URL } from "../../../config/api"; 
-
-export default async function submitClientForm(e, formData, setFormData) {
-  console.log('event:', e);
-  console.log('formData:', formData);
-  e.preventDefault();
-
-    const res = await fetch(`${API_URL}/admin/addClient`, {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(formData),
-      credentials: 'include',
-    });
-
-    const data = await res.json();
-    console.log('Response data:', data);
-    
-    alert(data.message || 'Submitted!');
-    setFormData(CLIENTFORMCONFIG); 
-} 
