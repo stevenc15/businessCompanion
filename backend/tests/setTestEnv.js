36bd03a8b8ad2e41bc4cf8648493f6b4cfff64c0
Steven <st201789@ucf.edu>
2026-03-20 22:30:35 -0400

    changed access to json file as access to a specific environment variable

--- backend/tests/setTestEnv.js
+++ backend/tests/setTestEnv.js
@@ -8,6 +8,7 @@ process.env.GOOGLE_CLIENT_ID = 'test-google-client-id';
 process.env.GOOGLE_CLIENT_SECRET = 'test-google-client-secret';
 process.env.GOOGLE_SHEET_ID = 'test-sheet-id';
 process.env.GOOGLE_SHEET_EMBED_URL = 'https://docs.google.com/spreadsheets/d/test/edit';
+process.env.GOOGLE_SERVICE_ACCOUNT_CREDENTIALS = JSON.stringify({ type: 'service_account', project_id: 'test' });
 process.env.SESSION_SECRET = 'test-session-secret';
 process.env.FRONTEND_URL = 'http://localhost:5173';
 process.env.BACKEND_URL = 'http://localhost:5001';
