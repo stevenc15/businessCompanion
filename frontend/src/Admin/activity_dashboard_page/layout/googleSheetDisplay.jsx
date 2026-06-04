34a807cc8f0062f8ce2e555e5218e2b044baec91
Steven <st201789@ucf.edu>
2026-06-04 14:17:45 -0500

    adjusting design for mobile display

--- frontend/src/Admin/activity_dashboard_page/layout/googleSheetDisplay.jsx
+++ frontend/src/Admin/activity_dashboard_page/layout/googleSheetDisplay.jsx
@@ -7,12 +7,9 @@ export default function GoogleSheetDisplay({sheetUrl, loading}) {
     if (loading) return <div className="spinner"></div>;
     if (!sheetUrl) return <p>Unable to load sheet.</p>;
     return (
-        <div>
+        <div className="sheet-iframe-wrapper">
             <iframe
                 src={sheetUrl}
-                width="100%"
-                height="600"
-                style={{ border: 'none' }}
                 title="Google Sheet"
             />
         </div>
